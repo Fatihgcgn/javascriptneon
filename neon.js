c52a94845a9a6c7100bea82194a92fd102f12cb7
Fatih İdris Geçgin <93212360+Fatihgcgn@users.noreply.github.com>
2022-08-22 15:49:12 +0300

    Add files via upload

---
+++ neon.js
@@ -0,0 +1,7 @@
+let greeting = document.querySelector("#greeting")
+greeting.addEventListener("click",domClick)
+
+function domClick() {
+    console.log("etkinlik denetlendi")
+    this.style.color == "red" ? this.style.color = "black" : this.style.color = "red"
+}
